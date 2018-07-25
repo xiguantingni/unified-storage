b1c6e9d17cd7281265824bc5ab5ebe5b3f4d4cf7
renchongchong <renchongchong@chinac.com>
2018-07-25 16:23:39 +0800

    自定义loading组件

--- src/component/loading/index.js
+++ src/component/loading/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by RCC on 2018/7/13.
- * 封装loading组件
+ * loading组件
  */
 
 import React from 'react';
@@ -8,35 +8,44 @@ import PropTypes from 'prop-types';
 import { Spin, Icon } from 'antd';
 import './index.css'
 
+const SIZE = {
+    small: { width: 50, height: 50 },
+    middle: { width: 100, height: 100 },
+    large: { width: 200, height: 200 }
+};
+
 class Loading extends React.Component {
     render() {
+        const { type, size, style,  className} = this.props;
         return (
-            <div style={this.props.style}>
-                <Spin />
-                <div className="loading-item-1"></div>
-                <div className="loading-item-2"></div>
-                <div className="loading-item-3"></div>
-                <div className="loading-item-4"></div>
-                <div className="loading-item-5"></div>
+            <div style={style} className={className + ' loading-container'}>
+                <div style={size || SIZE[type]} className="loading-view">
+                    <div className="item item-1"></div>
+                    <div className="item item-2"></div>
+                    <div className="item item-3"></div>
+                    <div className="item item-4"></div>
+                    <div className="item item-5"></div>
+                </div>
             </div>
         )
     }
-
 }
 
 Loading.propTypes = {
-    size: PropTypes.oneOf(['small', 'middle', 'large']),
-    customSize: PropTypes.shape({
+    type: PropTypes.oneOf(['small', 'middle', 'large']),
+    size: PropTypes.shape({
         width: PropTypes.number,
         height: PropTypes.number
     }),
-    style: PropTypes.func,
+    style: PropTypes.object,
     className: PropTypes.string
 };
 
 Loading.defaultProps = {
-    size: 'small',
-    customSize: {}
+    type: 'middle',
+    size: null,
+    style: {},
+    className: ''
 };
 
 export default Loading;
